2df6037ab37dfb1307df42a8a1ce00a469a25127
João Victor da Silva <joaovictordasilva0911@gmail.com>
2025-10-06 11:02:25 -0300

    feat: add `@tanstack/react-query`

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./app.tsx";
+import { Providers } from "./components/providers.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<App />
+		<Providers>
+			<App />
+		</Providers>
 	</StrictMode>,
 );
