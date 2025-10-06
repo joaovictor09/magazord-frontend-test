554be805246f7e2ea0c4b139c9953e449e61b0c6
João Victor da Silva <joaovictordasilva0911@gmail.com>
2025-10-06 14:31:50 -0300

    feat: extend Input component to include value and onChange props for better state management

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,9 +1,11 @@
 interface InputProps {
 	placeholder: string;
 	icon: React.ReactNode;
+	value: string;
+	onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export function Input({ placeholder, icon }: InputProps) {
+export function Input({ placeholder, icon, value, onChange }: InputProps) {
 	return (
 		<div className="border-b border-[#F4F4F4] space-x-4 flex items-center max-w-[444px] w-full">
 			{icon}
@@ -11,6 +13,8 @@ export function Input({ placeholder, icon }: InputProps) {
 				type="text"
 				placeholder={placeholder}
 				className="outline-none w-full text-lg px-0 py-1.5"
+				value={value}
+				onChange={onChange}
 			/>
 		</div>
 	);
