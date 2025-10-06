0af90602170fd4d91eaa75650635adb9e582c1f2
João Victor da Silva <joaovictordasilva0911@gmail.com>
2025-10-06 14:23:53 -0300

    feat: enhance GitHub service with pagination for repository and starred repository fetching

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,8 +1,11 @@
 import axios from "axios";
 
+const githubToken = import.meta.env.VITE_GITHUB_TOKEN;
+
 export const api = axios.create({
 	baseURL: "https://api.github.com",
 	headers: {
 		Accept: "application/vnd.github+json",
+		Authorization: githubToken ? `Bearer ${githubToken}` : undefined,
 	},
 });
--- src/services/github-service.ts
+++ src/services/github-service.ts
@@ -4,31 +4,47 @@ import type {
 	GithubUser,
 } from "../types/github-service-types";
 
+async function getPaginatedData(url: string): Promise<GithubRepository[]> {
+	const nextPattern = /(?<=<)([\S]*)(?=>; rel="next")/i;
+	let pagesRemaining = true;
+	let data: GithubRepository[] = [];
+
+	while (pagesRemaining) {
+		const response = await api.get<GithubRepository[]>(url, {
+			params: {
+				per_page: 100,
+			},
+		});
+
+		data = [...data, ...response.data];
+
+		const linkHeader = response.headers.link;
+
+		pagesRemaining = linkHeader && linkHeader.includes(`rel="next"`);
+
+		if (pagesRemaining) {
+			const match = linkHeader.match(nextPattern);
+			if (match) {
+				const nextUrl = new URL(match[0]);
+				url = nextUrl.pathname + nextUrl.search;
+			} else {
+				pagesRemaining = false;
+			}
+		}
+	}
+
+	return data;
+}
+
 export const githubService = {
 	getUser: async (username: string) => {
 		const response = await api.get<GithubUser>(`/users/${username}`);
 		return response.data;
 	},
 	getRepositories: async (username: string) => {
-		const response = await api.get<GithubRepository[]>(
-			`/users/${username}/repos`,
-			{
-				params: {
-					per_page: 1000,
-				},
-			},
-		);
-		return response.data;
+		return getPaginatedData(`/users/${username}/repos`);
 	},
 	getStarredRepositories: async (username: string) => {
-		const response = await api.get<GithubRepository[]>(
-			`/users/${username}/starred`,
-			{
-				params: {
-					per_page: 1000,
-				},
-			},
-		);
-		return response.data;
+		return getPaginatedData(`/users/${username}/starred`);
 	},
 };
