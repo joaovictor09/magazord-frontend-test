3ffac1fad1af3f681cbbb403f7df956be38684ef
João Victor da Silva <joaovictordasilva0911@gmail.com>
2025-10-06 15:32:03 -0300

    refactor: optimize GitHub service instantiation in query hooks for improved performance

--- src/queries/github/use-get-starred-repositories.ts
+++ src/queries/github/use-get-starred-repositories.ts
@@ -1,9 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
 import { GithubService } from "../../services/github-service";
 
+const githubService = new GithubService();
+
 export function useGetStarredRepositories(username: string) {
 	return useQuery({
 		queryKey: ["github", "starred", username],
-		queryFn: () => new GithubService().getStarredRepositories(username),
+		queryFn: () => githubService.getStarredRepositories(username),
 	});
 }
--- src/queries/github/use-get-user.ts
+++ src/queries/github/use-get-user.ts
@@ -1,9 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
 import { GithubService } from "../../services/github-service";
 
+const githubService = new GithubService();
+
 export function useGetUser(username: string) {
 	return useQuery({
 		queryKey: ["github", "user", username],
-		queryFn: () => new GithubService().getUser(username),
+		queryFn: () => githubService.getUser(username),
 	});
 }
