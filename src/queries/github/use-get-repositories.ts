d1f6fbff12c21cd2da8b77d461d5f67ca3022bdf
João Victor da Silva <joaovictordasilva0911@gmail.com>
2025-10-06 15:23:01 -0300

    refactor: update GitHub service usage in queries to instantiate GithubService class

--- src/queries/github/use-get-repositories.ts
+++ src/queries/github/use-get-repositories.ts
@@ -1,9 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
-import { githubService } from "../../services/github-service";
+import { GithubService } from "../../services/github-service";
 
 export function useGetRepositories(username: string) {
 	return useQuery({
 		queryKey: ["github", "repositories", username],
-		queryFn: () => githubService.getRepositories(username),
+		queryFn: () => new GithubService().getRepositories(username),
 	});
 }
--- src/queries/github/use-get-user.ts
+++ src/queries/github/use-get-user.ts
@@ -1,9 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
-import { githubService } from "../../services/github-service";
+import { GithubService } from "../../services/github-service";
 
 export function useGetUser(username: string) {
 	return useQuery({
 		queryKey: ["github", "user", username],
-		queryFn: () => githubService.getUser(username),
+		queryFn: () => new GithubService().getUser(username),
 	});
 }
